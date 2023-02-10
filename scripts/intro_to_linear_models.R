d4489a5198191f1930e138f22d3ad2f7cc17a322
Jim Massey <xne21ktu@uea.ac.uk>
2023-02-10 13:27:52 +0000

    answering darwins question and finding other variable mean

--- scripts/intro_to_linear_models.R
+++ scripts/intro_to_linear_models.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(GGally)
 library(emmeans)
 library(performance)
+library(broom.helpers)
+
 
 # Lm function ----
 lsmodel0 <- lm(formula = height ~ 1, data = darwin)
@@ -51,11 +53,51 @@ darwin %>%
 # Confidence intervals ----
 confint(lsmodel1) # intercept row gives a 95% CI for the height of the crossed plants,
                   # and the second row gives a 95% interval for the difference in height between crossed and selfed plants.
+
+# Answering the question ----
+
+# Darwin's original hypothesis was that self-pollination would reduce fitness (using height as a proxy for this)
+# The null hypothesis is that there is no effect of pollination type, and therefore no difference in the average heights.
+# we can simply determine whether or not the predicted value of our null hypothesis (a difference of zero),
+# lies inside the 95% CI for the difference of the mean.
+# In this case we can see that the upper and lower bounds of the confidence intervals do not cross zero.
+
+
+
  
+GGally::ggcoef_model(lsmodel1,
+                     show_p_values=FALSE, 
+                     conf.level=0.95)   # produces a graph of the estimated mean difference with an approx 95% CI.
+                                        # As we can see we are able to reject the null hypothesis at a 95% confidence level.
+                                        # when changing to 99% CI, the upper bound crosses zero - cannot rejct null
+broom::tidy(lsmodel1, conf.int=T, conf.level=0.99) # represents same argument but with 99% CI - cannot reject null
 
+# Getting other treatment mean and standard error ----
 
+darwin %>% 
+  mutate(type=factor(type)) %>% 
+  mutate(type=fct_relevel(type, c("Self", "Cross"))) %>% 
+  lm(height~type, data=.) %>% 
+  broom::tidy() # tells us mean and SE of type self
 
+# emmeans function ----
 
+means <- emmeans::emmeans(lsmodel1, specs = ~ type) # does similar thing to previous code
+
+means
+
+means %>% 
+  as_tibble() %>% 
+  ggplot(aes(x=type, 
+             y=emmean))+
+  geom_pointrange(aes(
+    ymin=lower.CL, 
+    ymax=upper.CL)) # can use emmeans as a handy summary to include in data visuals that combine raw data and statistical inferences.
+
+# as variance is not being calculated separately, and instead is assumed to be roughly equal (pooled estimate),
+# the standard error (and so 95% CI) are the same. This assumption still needs to be checked to trust results.
+
+# Assumption Checking ----
 
 
 
