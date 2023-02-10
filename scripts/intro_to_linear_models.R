1f3abcaa3b267a07f38da08d5dd29239e6376609
Jim Massey <xne21ktu@uea.ac.uk>
2023-02-10 12:57:30 +0000

    calculating SED and 95% CI

--- scripts/intro_to_linear_models.R
+++ scripts/intro_to_linear_models.R
@@ -11,6 +11,8 @@ summary(lsmodel0) #The 18.9 is the estimate of the model coefficient (in this ca
                   #together with its standard error. 
 mean(darwin$height) # can see that the intercept in the summay represents the mean in this case
 
+# Comparing means ----
+
 lsmodel1 <- lm(height ~ type, data=darwin) # analyses the difference in average plant height as a function of pollination type
 
 # note that the following is identical
@@ -29,12 +31,27 @@ darwin %>%
   summarise(mean=mean(height))
 # shows same difference in mean height
 
+# Standard error of the difference ----
 
+summary(lsmodel1) # tells us SED in the type self row (standard error difference) but our model assumes equal variance,
+                  # to take advantage of the larger sample size through 'pooling', to generate a more accurate SED.
+                  # But this assumption needs to be checked in our model
+darwin %>% 
+  ggplot(aes(x=type, 
+             y=height,
+             colour=type))+
+  geom_jitter(alpha=0.5,
+              width=0.1)+
+  stat_summary(fun=mean,
+               size=1.2)+
+  theme_bw()
 
+# plotting calculated means
 
-
-
-
+# Confidence intervals ----
+confint(lsmodel1) # intercept row gives a 95% CI for the height of the crossed plants,
+                  # and the second row gives a 95% interval for the difference in height between crossed and selfed plants.
+ 
 
 
 
