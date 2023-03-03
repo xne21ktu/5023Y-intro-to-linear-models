0d38d0773b34c88dd2c9c864f9124d29bc9b284a
Jim Massey <xne21ktu@uea.ac.uk>
2023-03-03 13:09:02 +0000

    Using two-way anova to explore two explanatory variables

--- scripts/anova.R
+++ scripts/anova.R
@@ -12,7 +12,7 @@ summary(lsmodel1)
 # The first column contains the information source, here the signal/regression (first row) and error/noise (second row).
 # The second column gives the df, in the first row this is the number of predictors(treatments) (k - 1, including intercept), and in the second row the residual degrees of freedom (N - k).
 # The third column is the sum of squares, in the first row this is the SSR (sum of square residuals), and the second row SSE (sum of squared error), (remember that SST = SSR + SSE).
-# The fourth column is the mean sum of squares ( average variability per treatment level), the first row is MSR (mean sum of sqaure residuals) and the second row is te MSE (mean sum of squared error). MSR = SSR/(k-1) & MSE = SSE/(N-k).
+# The fourth column is the mean sum of squares ( average variability per treatment level), the first row is MSR (mean sum of square residuals) and the second row is te MSE (mean sum of squared error). MSR = SSR/(k-1) & MSE = SSE/(N-k).
 # to generate MSE, the linear model has pooled the SSE from the two treatments, this gives it greater power for detecting effects with smaller sample sizes, but is the reason why we need homogeneity of variance as an assumption of our model.
 # The fifth column is our F statistic (the signal-to-noise ratio). MSR/MSE (treatment variance/residual error variance)
 # F = 5.9, which means that the estimated signal is nearly six times larger than the estimated noise.
@@ -20,3 +20,13 @@ summary(lsmodel1)
 pf(5.9395, 1, 28, lower.tail=FALSE)
 # Can use F test (pf function), taking into account sample size and df, to calculate the probability of observing this ratio of signal to noise if the null hypothesis that there were no effect of treatment is true
 # The self pollinated maize plants measured an average of 17.6 [16-19.1] (mean[95% CI]) inches high, while the cross-pollinated plants had a mean height of 20.2 [18.6-21.7] inches - a difference of 2.6 [-0.4-4.8] inches (one-way ANOVA: F1,28 = 5.9, P = 0.02).
+
+# Two-Way ANOVA ----
+# includes two explanatory variables. Usually these are treatments of interest, but for this example we will stick with including the pair variable.
+
+lsmodel2 <- lm(height ~ type + as.factor(pair), data = darwin)
+
+anova(lsmodel2)
+# Can see some of the degrees of freedom and SSE which were initially in the residuals, have now been 'used' by the new pair term (in the form of SSR) in the more complex model
+# Pairing has actually increased the relative proportion of SSE compared to SSR (implies a 'negative variance component').
+# When mean squares of the regression (6.2) are smaller than the residuals (11.1) it implies a problem with the experimental design. It could be a result of undersampling (more pairs are needed), however it could also be that these pairs were not sampled at random.
